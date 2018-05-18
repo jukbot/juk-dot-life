731f328afdd2b7625912902ece11693e679c7eef
jukbot <chukkrit.juk@gmail.com>
2018-05-18 19:00:43 +0700

    add service worker :sparkles:

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+"use_strict";
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "UA-115454106-2");
+
+if ("serviceWorker" in navigator && location.protocol === "https:") {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('Service Worker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('Service Worker registration failed: ', err);
+    });
+  });
+}
