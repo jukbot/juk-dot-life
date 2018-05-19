eeea66a4b5279e8d8e4ed744368b6b8e7bdbf3db
jukbot <chukkrit.juk@gmail.com>
2018-05-19 21:01:49 +0700

    fixed sw path

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ gtag("config", "UA-115454106-2");
 
 if ("serviceWorker" in navigator && location.protocol === "https:") {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registration was successful
       console.log('Service Worker registration successful with scope: ', registration.scope);
     }, function(err) {
